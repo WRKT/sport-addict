81d38fd3f99b44d6f8df710eab816ccae44f5c55
WinnessRakotozafy <winness.rakotozafy@outlook.fr>
2022-02-25 08:55:05 +0100

    Test Background-img no random

--- JS/main.js
+++ JS/main.js
@@ -1,13 +1,15 @@
 //Début fonction background-img aléatoire
 function randomImg() {
+  let i = 0;
     const randomImgLink = [
             `url(CSS/images/activites/basketball.jpg)`,
             `url(CSS/images/activites/boxe.jpg)`,
             `url(CSS/images/activites/crossfit.jpg)`,
             `url(CSS/images/activites/musculation.jpg)`,
         ]
-    document.getElementById("Presentation").style.backgroundImage=randomImgLink[Math.floor(Math.random() * randomImgLink.length)];
-    console.log("done")
+    let stylePresentation = document.querySelector("#Presentation");
+    let theCSSprop = window.getComputedStyle(stylePresentation,null).getPropertyValue("background-image");
+    console.log(theCSSprop);
   }
 
 setInterval(() => {randomImg();}, 4000)
