5fc7517e3df9e85a36840049be75f1072fcb8e34
WinnessRakotozafy <winness.rakotozafy@outlook.fr>
2022-02-25 12:06:08 +0100

    Design search input & add data for search

---
+++ JS/data.js
@@ -0,0 +1,159 @@
+export const IMAGES_DATA = [
+  {
+    "_id": "basketball",
+    "class": "activity",
+    "title": "Basketball",
+    "link": "./assets/images/activites/basketball.jpg",
+    "favlink": "../assets/images/activites/basketball.jpg",
+    "alt": "Basketball Image",
+    "comment": "Gérer le ballon",
+    "h1": "Le Basketball"
+  },
+  {
+    "_id": "boxe",
+    "class": "activity",
+    "title": "Boxe",
+    "link": "./assets/images/activites/boxe.jpg",
+    "favlink": "../assets/images/activites/boxe.jpg",
+    "alt": "Boxe Image",
+    "comment": "Mouvement de jambes et crochet du droit",
+    "h1": "La Boxe"
+  },
+  {
+    "_id": "crossfit",
+    "class": "activity",
+    "title": "Crossfit",
+    "link": "./assets/images/activites/crossfit.jpg",
+    "favlink": "../assets/images/activites/crossfit.jpg",
+    "alt": "Crossfit Image",
+    "comment": "Pour devenir un athlète complet",
+    "h1": "Le Crossfit"
+  },
+  {
+    "_id": "fitness",
+    "class": "activity",
+    "title": "Fitness",
+    "link": "./assets/images/activites/fitness.jpg",
+    "favlink": "../assets/images/activites/fitness.jpg",
+    "alt": "Fitness Image",
+    "comment": "Harmoniser son corps",
+    "h1": "Le Fitness"
+  },
+  {
+    "_id": "karaté",
+    "class": "activity",
+    "title": "Karaté",
+    "link": "./assets/images/activites/karaté.jpg",
+    "favlink": "../assets/images/activites/karaté.jpg",
+    "alt": "Karaté Image",
+    "comment": "Les katas pour éviter la cata",
+    "h1": "Le Karaté"
+  },
+  {
+    "_id": "musculation",
+    "class": "activity",
+    "title": "Musculation",
+    "link": "./assets/images/activites/musculation.jpg",
+    "favlink": "../assets/images/activites/musculation.jpg",
+    "alt": "Musculation Image",
+    "comment": "Aussi puissant qu'harmonieux",
+    "h1": "La Musculation"
+  },
+  {
+    "_id": "canoé",
+    "check_date": "canoé",
+    "class": "events",
+    "title": "Canoé",
+    "link": "./assets/images/evenement/canoe.jpg",
+    "favlink": "../assets/images/evenement/canoe.jpg",
+    "alt": "Canoe Image",
+    "comment": "Descente des chutes éternelles",
+    "h1": "Le Canoé"
+  },
+  {
+    "_id": "football",
+    "check_date": "football",
+    "class": "events",
+    "title": "Football",
+    "link": "./assets/images/evenement/football.jpg",
+    "favlink": "../assets/images/evenement/football.jpg",
+    "alt": "Football Image",
+    "comment": "Recrutement des nouveaux joueurs",
+    "h1": "Le Football"
+  },
+  {
+    "_id": "motocross",
+    "check_date": "motocross",
+    "class": "events",
+    "title": "Motocross",
+    "link": "./assets/images/evenement/motocross.jpg",
+    "favlink": "../assets/images/evenement/motocross.jpg",
+    "alt": "Motocross Image",
+    "comment": "Enduro de la veille de l'automne",
+    "h1": "La Motocross"
+  },
+  {
+    "_id": "planche à voile",
+    "this_element": "planche-à-voile",
+    "check_date": "planche-à-voile",
+    "class": "events",
+    "title": "Planche à voile",
+    "link": "./assets/images/evenement/plancheAvoile.jpg",
+    "favlink": "../assets/images/evenement/plancheAvoile.jpg",
+    "alt": "Planche à voile Image",
+    "comment": "Exhibition intercontinentale des masters Pro",
+    "h1": "La Planche à Voile"
+  },
+  {
+    "_id": "plongée",
+    "check_date": "plongée",
+    "class": "events",
+    "title": "Plongée",
+    "link": "./assets/images/evenement/plongée.jpg",
+    "favlink": "../assets/images/evenement/plongée.jpg",
+    "alt": "Plongée Image",
+    "comment": "Découverte des gorges insondables avec une équipe pro",
+    "h1": "La Plongée"
+  },
+  {
+    "_id": "triathlon",
+    "check_date": "triathlon",
+    "class": "events",
+    "title": "Triathlon",
+    "link": "./assets/images/evenement/triathlon.jpg",
+    "favlink": "../assets/images/evenement/triathlon.jpg",
+    "alt": "Triathlon Image",
+    "comment": "Ironman de Noël",
+    "h1": "Le Triathlon"
+  },
+  {
+    "_id": "natation",
+    "class": "populaire",
+    "title": "Natation",
+    "link": "./assets/images/populaire/natation.jpg",
+    "favlink": "../assets/images/populaire/natation.jpg",
+    "alt": "Natation Image",
+    "comment": "Force et justesse du mouvement",
+    "h1": "La Natation"
+  },
+  {
+    "_id": "running",
+    "class": "populaire",
+    "title": "Running",
+    "link": "./assets/images/populaire/running.jpg",
+    "favlink": "../assets/images/populaire/running.jpg",
+    "alt": "Running Image",
+    "comment": "Une paire de basket et à nous les grands espaces",
+    "h1": "La course à pied"
+  },
+  {
+    "_id": "yoga",
+    "class": "populaire",
+    "title": "Yoga",
+    "link": "./assets/images/populaire/yoga.jpg",
+    "favlink": "../assets/images/populaire/yoga.jpg",
+    "alt": "Yoga Image",
+    "comment": "Calme, méditation et musculation interne",
+    "h1": "Le Yoga" 
+  }
+]
